a1181383494f2b746fc672b7db028d26bb8c93f3
loganhull <logan.hull@hotmail.com>
2016-09-07 19:53:02 -0400

    completed the code challenge for week 2

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,24 @@
 //Call function
 
 //Alert user results
+
+
+function processMessage(msg)
+{
+    if(msg == "hello")
+        {
+            alert("hello world");
+        }
+    
+else
+    alert("you didnt say hello");
+    
+    
+    console.log(msg);
+}
+
+var message;
+
+message = prompt();
+
+processMessage(message);
